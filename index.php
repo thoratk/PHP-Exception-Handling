a463808e806050d1ab6991466130db795b78f46f
Kiran THORAT <kiran-deshmukh_student2021@wilder.school>
2022-01-01 20:01:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'Car.php';
+
+
+$myCar = new Car('blue', 8, 'gasoline');
+echo $myCar->checkError(true);
+echo "\n";
+var_dump($myCar->setParkBrake($myCar->checkError(true)));
+
+
